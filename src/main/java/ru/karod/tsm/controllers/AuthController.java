4cb579c8ac7d4689f78b5f3b696e0a2987440c6b
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-31 12:03:04 +0300

    deleted SubjectDTO, SubjectsResponse.java, added access to SubjectController

--- src/main/java/ru/karod/tsm/controllers/AuthController.java
+++ src/main/java/ru/karod/tsm/controllers/AuthController.java
@@ -1,21 +1,16 @@
 package ru.karod.tsm.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.karod.tsm.dto.SubjectDTO;
-import ru.karod.tsm.dto.SubjectsResponse;
 import ru.karod.tsm.security.request.LoginRequest;
 import ru.karod.tsm.security.request.SignupRequest;
 import ru.karod.tsm.services.AuthService;
-import ru.karod.tsm.services.SubjectService;
 import ru.karod.tsm.util.MessageResponse;
 
 import javax.validation.Valid;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,15 +19,6 @@ import java.util.stream.Collectors;
 @PreAuthorize("permitAll()")
 public class AuthController {
     private final AuthService authService;
-    private final SubjectService subjectService;
-    private final ModelMapper modelMapper;
-    @GetMapping("/subject")
-    public SubjectsResponse getSubjects() {
-        // оборачиваем список в объект для пересылки
-        return new SubjectsResponse(subjectService.findAll().stream()
-                .map(subject -> modelMapper.map(subject, SubjectDTO.class))
-                .collect(Collectors.toList()));
-    }
 
     @PostMapping("/sign_in")
     public ResponseEntity<Object> authenticateUser(@RequestBody @Valid
--- src/main/java/ru/karod/tsm/validations/UserValidator.java
+++ src/main/java/ru/karod/tsm/validations/UserValidator.java
@@ -1,21 +1,16 @@
 package ru.karod.tsm.validations;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ru.karod.tsm.models.User;
 import ru.karod.tsm.services.UserService;
 
-import java.util.Optional;
-
 @Component
 @RequiredArgsConstructor
-
 public class UserValidator implements Validator {
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public boolean supports(Class<?> clazz) {
