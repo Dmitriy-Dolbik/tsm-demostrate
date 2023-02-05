b09d74a5f4165b673d2b970374250d4f0fc3446d
Dmitrii Dolbik <dda1508@gmail.com>
2023-02-05 23:20:27 +0300

    Add getting an public IP via http://checkip.amazonaws.com

---
+++ src/main/java/ru/karod/tsm/exceptions/GettingIPAddressException.java
@@ -0,0 +1,8 @@
+package ru.karod.tsm.exceptions;
+
+public class GettingIPAddressException extends RuntimeException
+{
+    public GettingIPAddressException(String message){
+        super(message);
+    }
+}
--- src/main/java/ru/karod/tsm/util/impl/TsmUserUtilImpl.java
+++ src/main/java/ru/karod/tsm/util/impl/TsmUserUtilImpl.java
@@ -1,6 +1,10 @@
 package ru.karod.tsm.util.impl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 import javax.validation.constraints.NotNull;
@@ -8,6 +12,8 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import ru.karod.tsm.exceptions.GettingIPAddressException;
+import ru.karod.tsm.exceptions.ReadingFileException;
 import ru.karod.tsm.models.User;
 import ru.karod.tsm.util.UserUtil;
 
@@ -27,15 +33,21 @@ public class TsmUserUtilImpl implements UserUtil
     public String getVerifyURLForUser(@NotNull final User user){
 
         String hostname = null;
+        URL ipURL = null;
+        String serverPublicIP = null;
         try
         {
-            hostname = InetAddress.getLocalHost().getHostAddress();
+            ipURL = new URL("http://checkip.amazonaws.com");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(ipURL.openStream()))){
+                serverPublicIP = in.readLine();
+            }
         }
-        catch (UnknownHostException e)
+        catch (IOException e)
         {
-            throw new RuntimeException(e);
+            throw new GettingIPAddressException(String.format(
+                    "Error during getting an IP adress via http://checkip.amazonaws.com, message: [%s]", e.getMessage()));
         }
-        String serverURL = "http://" + hostname + ":" + serverPort;
+        String serverURL = "http://" + serverPublicIP + ":" + serverPort;
         return serverURL + registrationPostfix + user.getVerificationCode();
     }
 
